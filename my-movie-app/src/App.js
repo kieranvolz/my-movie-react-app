0fe12f4bfa6cf73bb34363382655140a5c73e4ac
Kieran Volz <kieran.volz@gmail.com>
2022-10-22 09:15:59 -0400

    running into an issue with font awesome
    putting in a commit here in case i have to go back

--- my-movie-app/src/App.js
+++ my-movie-app/src/App.js
@@ -1,38 +1,6 @@
-// import logo from './logo.svg';
 import './App.css';
-// import './index.css';
 import React from 'react';
 import axios from 'axios';
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
-// function App() {
-//   return (
-
-
 
 
 class MoviesList extends React.Component {
@@ -80,7 +48,8 @@ class MoviesList extends React.Component {
                       onChange={this.handleChange}
                   />
                   <button type="submit">
-                      <i className="fa fa-search" />
+                  {/* <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> */}
+                                        {/* <i className="fa fa-search" /> */}
                   </button>
               </form>
               {moviesList.length > 0 ? (
